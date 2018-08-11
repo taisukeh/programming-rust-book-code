0a0a0231e4a7075fbfdc391ee54ddc71eb465128
Taisuke Hori <taisuke.horix@gmail.com>
2018-08-11 19:22:35 +0900

    Add gcd

---
+++ gcd/gcd.rs
@@ -0,0 +1,36 @@
+use std::io::Write;
+use std::str::FromStr;
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+
+    while m != 0 {
+        if m < n {
+            std::mem::swap(&mut m, &mut n);
+        }
+
+        m = m % n;
+    }
+    n
+}
+
+fn main() {
+    let mut numbers = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+                     .expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in numbers.iter().skip(1) {
+        d = gcd(d, *m);
+    }
+
+    println!("The gcd of {:?} is {}", numbers, d);
+}
