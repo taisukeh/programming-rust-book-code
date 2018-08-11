9fc7207b9b94e9dc52c92dd9abf8ebdfbbd9e076
Taisuke Hori <taisuke.horix@gmail.com>
2018-08-12 01:02:59 +0900

    With Rayon

---
+++ mandelbrot-rayon/src/main.rs
@@ -0,0 +1,158 @@
+extern crate num;
+use num::Complex;
+use std::str::FromStr;
+extern crate image;
+use image::png::PNGEncoder;
+use image::ColorType;
+use std::fs::File;
+extern crate rayon;
+use rayon::prelude::*;
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10x20", 'x'), Some((10, 20)));
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
+}
+
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (w, h) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * w / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * h / bounds.1 as f64,
+    }
+}
+
+fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            };
+        }
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(
+        pixel_to_point(
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex { re: -0.5, im: -0.5 }
+    );
+}
+
+fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let mut p: Vec<u8> = vec![0; bounds.0 * bounds.1 * 3];
+    for (i, v) in pixels.iter().enumerate() {
+        p[i * 3 + 0] = (((*v >> 0) % 8) as f32 / 8.0 * 255.0) as u8;
+        p[i * 3 + 1] = (((*v >> 3) % 8) as f32 / 8.0 * 255.0) as u8;
+        p[i * 3 + 2] = (((*v >> 3) % 8) as f32 / 8.0 * 255.0) as u8;
+    }
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&p, bounds.0 as u32, bounds.1 as u32, ColorType::RGB(8))?;
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usageg: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT");
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing imageg dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels: Vec<u8> = vec![0; bounds.0 * bounds.1];
+
+    {
+        let bands: Vec<(usize, &mut [u8])> = pixels.chunks_mut(bounds.0).enumerate().collect();
+        bands.into_par_iter().for_each(|(i, band)| {
+            let top = i;
+            let band_bounds = (bounds.0, 1);
+            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_low_right =
+                pixel_to_point(bounds, (bounds.0, top), upper_left, lower_right);
+            render(band, band_bounds, band_upper_left, band_low_right);
+        });
+    }
+
+    write_image(&args[1], &pixels, bounds).expect("error writingg PNG file");
+}
